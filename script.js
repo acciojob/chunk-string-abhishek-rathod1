e5aa4b72fc1d9c5bb5edc167569fb3e6a19991d1
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-13 22:18:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function stringChop(str, size) {
   // your code here
+	if (!str) return [];
+	size = Number(size);
+	if (isNaN(size) || size <= 0) return [str]; // handle invalid sizes
+	if (size <= 0) return [str]; // invalid size fallback
+	
+	let result =[];
+	for(let i = 0; i<str.length; i=i+size){
+		result.push(str.slice(i,i+size));
+	}
+	return result;
 }
 
-// Do not change the code below
+// Do not change the code belowh
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
